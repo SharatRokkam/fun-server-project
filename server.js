9b09ec0cc909b63bd0fbdccd6c768f1ca0e8cbf6
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-04 15:42:04 +0530

    funserver

---
+++ server.js
@@ -0,0 +1,27 @@
+// Common JS module
+// require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to flipkart");
+});
+
+app.get("/mobile", (req, res) => {
+  res.send("<h2>Mobile brands are here </h2>");
+});
+
+app.get("/laptop", (req, res) => {
+  res.send("<h2>Laptop brands are here </h2>");
+});
+
+app.get("/charger", (req, res) => {
+  res.send("<h2>Charger brands are here </h2>");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port 3000`);
+});
